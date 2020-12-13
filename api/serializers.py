2157b89bfa1436305e51f3ebfe5f5ccd9184dae2
akhmetbekali <akhmetbek.ali@gmail.com>
2020-12-13 16:29:58 +0300

    POST request

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,6 @@ from api.models import Timer
 
 class TimerRequestSerializer(serializers.Serializer):
     json = serializers.JSONField()
+
+    def create(self, validated_data):
+        return Timer.objects.create(**validated_data)
--- api/views.py
+++ api/views.py
@@ -2,11 +2,14 @@ import json
 
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from api.models import Timer
 from api.serializers import TimerRequestSerializer
 
-
+@api_view(('GET', 'POST'))
 @csrf_exempt
 def timers_list(request):
     if request.method == 'GET':
@@ -14,12 +17,12 @@ def timers_list(request):
         serializer = TimerRequestSerializer(timers, many=True)
         return JsonResponse([t["json"] for t in serializer.data], safe=False)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TimerRequestSerializer(data=data)
+        a = request.body.decode("utf-8")
+        serializer = TimerRequestSerializer(data=json.loads(a))
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def timer_detail(request, pk):
     try:
